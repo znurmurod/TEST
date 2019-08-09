8b51077c25f9cabc33531790385d74f7743933bb
Nurmurod <nurmurod@saip.uz>
2019-08-09 17:42:14 +0500

    Third commit

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,37 +1,58 @@
-
 'usestrict';
 
-   let time;
-   let money;
- 
+
+
+let money = +prompt("Ваш бюджет за месяц", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+
+let appData = {
+   finance: money,
+   timeData: time,
+   expenses: {},
+   optionalexpenses: {},
+   income: [],
+   savings: false
+
+};
+
+
+// for (let i = 0; i < 2; i++) {
+//    let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+//    let b = +prompt("Во сколько обойдется?", "");
+
+//    if (typeof(a) === 'string' && typeof (a) != null && a != '' && a.length < 50 &&
+//       typeof(b) != null && b != '') {
+//       console.log("DONE");
+//       appData.expenses[a] = b;
+
+//    } else {
+//       i--;  
+// }
+// }
+let i = 0;
+do{
+   let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+   let b = +prompt("Во сколько обойдется?", "");
+
+   if (typeof (a) === 'string' && typeof (a) != null && a != '' && a.length < 50 &&
+      typeof (b) != null && b != '') {
+      console.log("DONE");
+      appData.expenses[a] = b;
+      i++;
+         } 
+}
+while (i < 2) 
    
-   let expenses = {
- 
-    firstQuestion: "",
-    secondQuestion: ""
-
-   };
-   let appData =  {
-       finance:money,
-      timeData:time,
-      expenses: expenses,
-      optionalexpenses: null ,
-      income: [],
-      savings: false
-   
-   };
-
-     money = prompt("Ваш бюджет за месяц","");
-     time = prompt("Введите дату в формате YYYY-MM-DD","");
-    expenses.firstQuestion = prompt("Введите обязательную статью расходов в этом месяце","");
-    expenses.secondQuestion = +prompt("Во сколько обойдется?","");
-     
-    let x = money/30;
-
-    alert("Ваш бюджет за день="+x);
-    
-
-    
 
 
-  
+appData.Permoney = appData.finance / 30;
+alert("Ваш бюджет за день=" + appData.Permoney);
+
+if (appData.Permoney < 100) {
+   console.log("Small profit");
+} else if (appData.Permoney > 100 && appData.Permoney < 1000) {
+   console.log("Average profit");
+} else {
+   console.log("NaN");
+}
